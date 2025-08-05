d7a445687f69f48e9acbaff660501c19aa651975
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2025-08-05 10:53:20 -0300

    atividade de strings

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -2,8 +2,14 @@
 # - Iniciar sempre com letra minúscula;
 # - O nome pode ser composto por letras, algarismos e underlines;
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
+print('Olá, Mundo)
 
 # Declare variáveis do tipo string
+Passaro = 'Arara'
+Nome = "Paulo"
+Paragrafo = 
+'''Rato roeu
+Roupa de rei de roma'''
 
 # Declare variáveis de cada tipo numérico
 
