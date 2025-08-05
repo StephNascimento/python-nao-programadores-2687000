3e83f8984482e483b835367870dc7cd687743eb0
StephNascimento <69490116+StephNascimento@users.noreply.github.com>
2025-08-05 10:54:53 -0300

    finalizando exercícios

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -2,16 +2,19 @@
 # - Iniciar sempre com letra minúscula;
 # - O nome pode ser composto por letras, algarismos e underlines;
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
-print('Olá, Mundo)
+print('Olá, Mundo')
 
 # Declare variáveis do tipo string
-Passaro = 'Arara'
-Nome = "Paulo"
-Paragrafo = 
+passaro = 'Arara'
+nome = "Paulo"
+paragrafo = 
 '''Rato roeu
 Roupa de rei de roma'''
 
 # Declare variáveis de cada tipo numérico
-
+num_inteiro = 10
+num_float = 2.5
 
 # Declare uma variável do tipo booleano
+trabalhando = True
+descansado = False
