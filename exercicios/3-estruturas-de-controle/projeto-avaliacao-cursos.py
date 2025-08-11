0d3a3a8104e2e2e197432d2d8c0884dc9bebb2b6
Stephanny Nascimento <69490116+StephNascimento@users.noreply.github.com>
2025-08-11 11:48:23 -0300

    Update projeto-avaliacao-cursos.py

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -5,3 +5,31 @@
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+
+cursos_linkedin = ["python", "sql", "excel", "powerbi", "comunicacao"]
+
+py = "python"
+xlsx = "excel"
+bi = "powerbi"
+
+cursos_notas = {}
+
+if py in cursos_linkedin:
+    print(f"O curso {py} está na lista.")
+    nota_py = int(input(f"Qual a nota para o curso {py}? "))
+    cursos_notas[py] = nota_py
+    
+if xlsx in cursos_linkedin:
+    print(f"O curso {xlsx} está na lista.")
+    nota_xlsx = int(input(f"Qual a nota para o curso {xlsx}? "))
+    cursos_notas[xlsx] = nota_xlsx
+    
+if bi in cursos_linkedin:
+    print(f"O curso {xlsx} está na lista.")
+    nota_bi = int(input(f"Qual a nota para o curso {bi}? "))
+    cursos_notas[xlsx] = nota_bi
+    
+else:
+    print("Curso não contém na lista")
+
+print("Notas dos cursos:", cursos_notas)
