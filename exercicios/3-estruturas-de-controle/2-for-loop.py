6337f1fc9d66f3fa00c5f0426da34066d8565410
Stephanny Nascimento <69490116+StephNascimento@users.noreply.github.com>
2025-08-11 11:55:22 -0300

    Update 2-for-loop.py

--- exercicios/3-estruturas-de-controle/2-for-loop.py
+++ exercicios/3-estruturas-de-controle/2-for-loop.py
@@ -1,9 +1,17 @@
 # Crie uma lista
+lista = ["banana", "maçã", "pera", "uva", "batata", "alho"]
 
 # Crie um for loop para imprimir cada elemento dessa lista
-
+for fruta in lista:
+    print(fruta)
 
 # Crie um objeto iterável utilizando a função range()
-
+list(range(7))
+list(range(2,40,2))
 
 # Use esse objeto iterável para criar um for loop que imprima na tela a frase "Estou aprendendo uma linguagem de programação."
+for num in range(7):
+    print(f"{num}  - Estou aprendendo uma linguagem de programação")
+
+for item in list(range(2,40,2)):
+    print(item, "- Estou aprendendo uma linguagem de programação")
